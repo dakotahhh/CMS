6ed386595cb03344ce08d4ab7a6b9467c03e6959
Dakotah <dreyes@student.neumont.edu>
2014-04-28 22:27:57 -0600

    added image and video things

---
+++ hellorest-server/src/main/java/edu/neumont/csc380/hello/service/ImageService.java
@@ -0,0 +1,37 @@
+package edu.neumont.csc380.hello.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+
+@Path("/image")
+public interface ImageService {
+
+	@GET
+	@Path("/{id")
+	@Produces({"image/jpg", "image/png"})
+	Response getImage(@PathParam("id") Long id);
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes({"image/jpg", "image/png"})
+	@Produces("application/json")
+	Response updateImage(@PathParam("id") Long id, Image image);
+	
+	@POST
+	@Consumes({"image/jpg", "image/png"})
+	@Produces("application/json")
+	Response createImage(Image image);
+	
+	@DELETE
+	@Path("/{id}")
+	Response deleteImage(@PathParam("id") Long id);
+
+}
---
+++ hellorest-server/src/main/java/edu/neumont/csc380/hello/service/VideoService.java
@@ -0,0 +1,37 @@
+package edu.neumont.csc380.hello.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+
+@Path("/video")
+public interface VideoService {
+
+	@GET
+	@Path("/{id}")
+	@Produces({"video/quicktime", "video/avi", "video/mp4"})
+	Response getVideo(@PathParam("id") Long id);
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes({"video/quicktime", "video/avi", "video/mp4"})
+	@Produces("application/json")
+	Response updateVideo(@PathParam("id") Long id, Video video);
+	
+	@POST
+	@Consumes({"video/quicktime", "video/avi", "video/mp4"})
+	@Produces("application/json")
+	Response createVideo(Video video);
+	
+	@DELETE
+	@Path("/{id}")
+	Response deleteVideo(@PathParam("id") Long id);
+
+}
